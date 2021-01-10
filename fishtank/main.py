eb83854c66b7feb800e4620103ffea9ae6340a42
Ben Hagan <benhagan100@gmail.com>
2021-01-10 15:37:22 -0500

    Told web page to refresh every 60 seconds

--- fishtank/main.py
+++ fishtank/main.py
@@ -29,6 +29,7 @@ def web_page():
   temp = read_ds_sensor()
   html = """<!DOCTYPE HTML><html><head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="refresh" content="60">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <style> html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center; }
     h2 { font-size: 3.0rem; } p { font-size: 3.0rem; } .units { font-size: 1.2rem; } 
